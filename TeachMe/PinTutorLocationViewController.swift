fe5361b07db3bfe31e210c099c6545bce49a2608
vuminhkhang1995 <vuminhkhang1995@gmail.com>
2015-09-19 18:47:24 +0700

    Initial Setups

---
+++ TeachMe/PinTutorLocationViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PinTutorLocationViewController.swift
+//  TeachMe
+//
+//  Created by Clover on 9/19/15.
+//  Copyright (c) 2015 Great White Shark. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PinTutorLocationViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func onCancel(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    
+    @IBAction func onSetLocation(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
--- TeachMe/SearchTutorViewController.swift
+++ TeachMe/SearchTutorViewController.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
-class SearchTutorViewController: UIViewController {
+class SearchTutorViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,14 +24,15 @@ class SearchTutorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    // - Search Bar
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        self.searchBar.endEditing(true)
+    }
     
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+       
     }
     
 }
