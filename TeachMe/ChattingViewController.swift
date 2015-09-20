186d0ac7d7ae3924cd42342f15d417a9ff7b0d78
vuminhkhang1995 <vuminhkhang1995@gmail.com>
2015-09-21 00:13:59 +0700

    Initial Setups

---
+++ TeachMe/ChattingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ChattingViewController.swift
+//  TeachMe
+//
+//  Created by Clover on 9/20/15.
+//  Copyright (c) 2015 Great White Shark. All rights reserved.
+//
+
+import UIKit
+
+class ChattingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        customizeNavBar()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func customizeNavBar() {
+        self.navigationItem.title = "Tutor Results"
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TeachMe/TutorDetailsViewController.swift
+++ TeachMe/TutorDetailsViewController.swift
@@ -1,24 +1,29 @@
 //
-//  SearchTutorsViewController.swift
+//  TutorDetailsViewController.swift
 //  TeachMe
 //
-//  Created by Clover on 9/17/15.
+//  Created by Clover on 9/20/15.
 //  Copyright (c) 2015 Great White Shark. All rights reserved.
 //
 
 import UIKit
-import MapKit
 
-class SearchTutorsViewController: UIViewController {
 
-    @IBOutlet weak var mapView: MKMapView!
+class TutorDetailsViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var aboutTutorLabel: UILabel!
+    
     override func viewWillAppear(animated: Bool) {
         navigationItem.title = "Teach Me"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //aboutTutorLabel.preferredMaxLayoutWidth = scrollView.frame.width
+
+        scrollView.contentSize.height = aboutTutorLabel.frame.height
         
     }
 
@@ -27,11 +32,13 @@ class SearchTutorsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: - Navigation
 
+    
+    // MARK: - Navigation
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Remove the back button's title by replacing the previous navigation bar's title
         navigationItem.title = ""
     }
-    
+
 }
